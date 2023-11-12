aef53e2ad0a5a87d885bbb4335c60a4cbfb8a410
Mykel-Eze <michaeleze1997@gmail.com>
2023-11-12 17:38:18 +0100

    NewsletterBlock done

--- src/Externals.vue
+++ src/Externals.vue
@@ -4,6 +4,7 @@
         <div id="main-contents-wrapper">
           <router-view />
         </div>
+      <NewsletterBlock />
       <Footer />
     </div>
   </template>
@@ -11,10 +12,12 @@
   <script>
   import Navbar from "@/components/Navbar.vue";
   import Footer from "@/components/Footer.vue";
+  import NewsletterBlock from './components/NewsletterBlock.vue';
+  
   export default {
     name: "ExternalLayout",
     components: {
-      Navbar, Footer
+      Navbar, Footer, NewsletterBlock
     }
   }
   </script>
